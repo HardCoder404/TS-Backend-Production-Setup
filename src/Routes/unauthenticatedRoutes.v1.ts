75a3e3e11c299fd6d6720f42b609d8f00b839dae
Prince Raj <127084297+HardCoder404@users.noreply.github.com>
2025-12-12 00:10:54 +0530

    Add test router to unauthenticated routes

--- src/Routes/unauthenticatedRoutes.v1.ts
+++ src/Routes/unauthenticatedRoutes.v1.ts
@@ -2,10 +2,14 @@ import { Router } from 'express'
 import { loginRateLimit } from '../middleware/rateLimit'
 import authRouter from '../app/Router/public/auth.routes'
 import pass from '../app/Router/public/password.routes'
+import testRouter from '../app/Router/public/test.routes'
+
 
 const router = Router()
 
 router.use('/auth', loginRateLimit, authRouter)
 router.use('/pass', loginRateLimit, pass)
+router.use('/test', testRouter)
+
 
 export default router
